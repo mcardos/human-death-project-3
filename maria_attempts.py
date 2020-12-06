b21a0202d93b63ca028fa1831048e4dfd674c4ba
Maria Cardoso <mcardoso1@babson.edu>
2020-12-05 21:55:04 -0500

    Add pictures to Images

--- maria_attempts.py
+++ maria_attempts.py
@@ -1,13 +1,45 @@
 import pygame
+import time 
 
-# Step 1: Initialize Game Screen
+# Step 1: Initialize Game Screen.
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Healthfy')
-# Add background
+
+# Add colors.
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (27, 133, 27)
+
+# Add background.
 background = pygame.image.load("/home/mcardoso/project-3-healthfy/Images/background.jpg")
+humanoid = pygame.image.load("/home/mcardoso/project-3-healthfy/Images/humanoid.jpg")
 clock = pygame.time.Clock()
 RUNNING = True
+current_score = 0
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()         
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects("Click one of the buttoms", smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
+
+def action():
+    pass 
+
+def score():
+     pass
+
 while RUNNING:
     # clock.tick(240)
     screen.fill((255, 255, 255))
